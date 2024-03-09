2b61d0b2d77a53207007098657143abd950a5058
--Shubham Jain <shubhamjain0594@gmail.com>
2024-03-09 20:32:36 +0000

    Changed streamlit.

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from st_click_detector import click_detector
 import base64
 
+import time
+
 
 def convert_image(image_path):
     with open(
@@ -22,84 +24,96 @@ result = ["Template1"] * num_slides
 
 
 def slide_row(image_paths):
-    h1_html = "<h1>Slides</h1>"
+    clickable_element = ""
     for index, image_path in enumerate(image_paths):
         imageBase64 = convert_image(image_path)
-        image_src = f'data:image/png;base64,{imageBase64}'
-        h1_html += f'<span style="padding-left:20px, margin-bottom:10px"> <a href="#" id="{index}"><img  src="{image_src}" alt="Image" width="100"></a></span>'
-
-    h1_tag = f"<h1 style='overflow-X: auto; text-align: center; display: grid; grid-template-rows:repeat(auto-fit, minmax(200px, 1fr));grid-gap: 10px;'>{h1_html}</h1>"
-    return h1_tag
+        image_src = f"data:image/png;base64,{imageBase64}"
+        clickable_element += f'<span style="padding-left:20px, margin-bottom:10px"> <a href="#" id="{index}"><img  src="{image_src}" alt="Image" width="100"></a></span>'
+    return clickable_element
 
 
-def main():
-    st.title("Document Upload App")
+def document_upload_app(placeholder):
+    """Show document upload app."""
+    placeholder.title("Document Upload App")
 
-    # Placeholder for Page 1 content
-    page1_placeholder = st.empty()
+    uploaded_file = placeholder.file_uploader("Upload a document", type=["txt", "pdf"])
 
-    # Placeholder for Page 2 content
-    page2_placeholder = st.empty()
+    # Simulate processing time if a file is uploaded
+    if uploaded_file is not None:
+        placeholder.write("File uploaded")
+        st.session_state.show_file_uploader = False
 
-    # Boolean flag to control visibility of file uploader
-    show_file_uploader = True
+        # Simulate processing time
+        time.sleep(0.03)
+        # progress_placeholder = st.progress(100)
+        st.session_state.loading_complete = True
 
-    # Page 1: Document Upload
-    if show_file_uploader:
-        uploaded_file = page1_placeholder.file_uploader(
-            "Upload a document", type=["txt", "pdf"]
-        )
 
-        # Simulate processing time if a file is uploaded
-        if uploaded_file is not None:
-            show_file_uploader = False
-            # Simulate processing time
-            import time
+def slides_app(placeholder):
+    placeholder.write("# Selected slides")
+    content = slide_row(image_paths)
+    slides_placeholder = click_detector(content, key="selected_slide")
 
-            time.sleep(0.03)
-            progress_placeholder = st.progress(100)
+    if slides_placeholder:
+        st.session_state.selected_index = slides_placeholder
+        st.session_state.selected_template = None
 
-            st.session_state.loading_complete = True
-
-    # Page 2: Hello World (displayed after processing is completed)
-    if getattr(st.session_state, "loading_complete", False):
-        
-        # Hide Page 1 content
-        page1_placeholder.empty()
-        progress_placeholder.empty()
-
-        page2_placeholder.header("Creating your presentation ...")
-        
-        content = slide_row(image_paths)
-        slides_placeholder = click_detector(content)
-
-        selected_index = 0
-        if(slides_placeholder):
-            selected_index = slides_placeholder
-        
+    if st.session_state.selected_index is not None:
         template1Image = convert_image("./images/image1.png")
         template2Image = convert_image("./images/image2.png")
         template3Image = convert_image("./images/image3.png")
 
-        template= click_detector(
-            f"<a href='#' id='Template1'><img width='100' src='data:image/png;base64,{template1Image}'></a><a href='#' id='Template2'><img width='100' src='data:image/png;base64,{template2Image}'></a><a href='#' id='Template2'><img width='100' src='data:image/png;base64,{template3Image}'></a>"
+        st.session_state.selected_template = click_detector(
+            f"<a href='#' id='Template1'><img width='100' src='data:image/png;base64,{template1Image}'></a>"
+            f"<a href='#' id='Template2'><img width='100' src='data:image/png;base64,{template2Image}'></a>"
+            f"<a href='#' id='Template3'><img width='100' src='data:image/png;base64,{template3Image}'></a>",
+            key="selected_template_index",
         )
 
         # Handle button clicks
-        index = int(selected_index)
-        print(f'index = {index}')
-        print(f'template= {template}')
-        
-        if template == 'Template1':
-            st.session_state.result[index] = 'Template 1'
-        elif template == 'Template2':
-            st.session_state.result[index] = 'Template 2'
-        elif template == 'Template3':
-            st.session_state.result[index] = 'Template 3'
+        index = int(st.session_state.selected_index)
+        st.write(f"index = {index}")
+        st.write(f"template= {st.session_state.selected_template}")
+        st.write("-------")
+
+        if st.session_state.selected_template is not None:
+            template = st.session_state.selected_template
+            if template == "Template1":
+                st.session_state.result[index] = "Template 1"
+            elif template == "Template2":
+                st.session_state.result[index] = "Template 2"
+            elif template == "Template3":
+                st.session_state.result[index] = "Template 3"
 
         st.write(st.session_state.result)
         print(st.session_state.result)
-    
+
+
+def main():
+    # Page 1: Document Upload
+    page_1_placeholder = st.empty()
+    page_2_placeholder = st.empty()
+    if st.session_state.show_file_uploader:
+        document_upload_app(page_1_placeholder)
+    # Page 2: Hello World (displayed after processing is completed)
+    if st.session_state.loading_complete:
+        page_1_placeholder.empty()
+        print(st.session_state)
+        page_2_placeholder.header("Creating your presentation ...")
+        slides_app(page_2_placeholder)
+
+
+def initialize_page():
+    st.write("Initializing...")
+    st.session_state.show_file_uploader = True
+    st.session_state.initialized = True
+    st.session_state.loading_complete = False
+    st.session_state.selected_index = None
+    st.session_state.selected_template = None
+    st.session_state.result = ["Template 1", "Template 2", "Template 3"]
+
+
 if __name__ == "__main__":
-    st.session_state.result = ['Template 1', 'Template 2', 'Template 3']
+    if getattr(st.session_state, "initialized", None) is None:
+        initialize_page()
     main()
