5d1579929918356a834db51ceb8db9987404dc5a
--Shubham Jain <shubhamjain0594@gmail.com>
2024-03-09 22:55:20 +0000

    Fixed streamlit for loading

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -1,3 +1,4 @@
+from typing import Dict
 import streamlit as st
 from st_click_detector import click_detector
 import base64
@@ -13,14 +14,10 @@ def convert_image(image_path):
         return base64.b64encode(image_file.read()).decode("utf-8")
 
 
-image_paths = [
-    "images/image1.png",
-    "images/image2.png",
-    "images/image3.png",
-]
-
 num_slides = 3
 result = ["Template1"] * num_slides
+
+image_name_to_path = {"image_1": "images/image1.png", "image_2": "images/image2.png", "image_3": "images/image3.png"}
 
 
 def slide_row(image_paths):
@@ -28,19 +25,19 @@ def slide_row(image_paths):
     for index, image_path in enumerate(image_paths):
         imageBase64 = convert_image(image_path)
         image_src = f"data:image/png;base64,{imageBase64}"
-        clickable_element += f'<span style="padding-left:20px, margin-bottom:10px"> <a href="#" id="{index}"><img  src="{image_src}" alt="Image" width="100"></a></span>'
+        clickable_element += f'<a href="#" id="{index}" ><img  src="{image_src}" alt="Image" width="120px" style="padding-right:10px"></a>'
     return clickable_element
 
 
-def document_upload_app(placeholder):
+def document_upload_app():
     """Show document upload app."""
-    placeholder.title("Document Upload App")
+    st.title("Document Upload App")
 
-    uploaded_file = placeholder.file_uploader("Upload a document", type=["txt", "pdf"])
+    uploaded_file = st.file_uploader("Upload a document", type=["txt", "pdf"])
 
     # Simulate processing time if a file is uploaded
     if uploaded_file is not None:
-        placeholder.write("File uploaded")
+        st.write("File uploaded")
         st.session_state.show_file_uploader = False
 
         # Simulate processing time
@@ -49,41 +46,39 @@ def document_upload_app(placeholder):
         st.session_state.loading_complete = True
 
 
-def slides_app(placeholder):
-    placeholder.write("# Selected slides")
-    content = slide_row(image_paths)
-    slides_placeholder = click_detector(content, key="selected_slide")
+def get_template_content(template_data: Dict) -> str:
+    element = ""
+    for key, val in template_data.items():
+        element += f"<a href='#' id='{key}'><img width='100' src='data:image/png;base64,{convert_image(val)}'></a>"
+    return element
 
-    if slides_placeholder:
-        st.session_state.selected_index = slides_placeholder
-        st.session_state.selected_template = None
 
-    if st.session_state.selected_index is not None:
-        template1Image = convert_image("./images/image1.png")
-        template2Image = convert_image("./images/image2.png")
-        template3Image = convert_image("./images/image3.png")
+def slides_app():
+    st.write("# Selected slides")
 
-        st.session_state.selected_template = click_detector(
-            f"<a href='#' id='Template1'><img width='100' src='data:image/png;base64,{template1Image}'></a>"
-            f"<a href='#' id='Template2'><img width='100' src='data:image/png;base64,{template2Image}'></a>"
-            f"<a href='#' id='Template3'><img width='100' src='data:image/png;base64,{template3Image}'></a>",
-            key="selected_template_index",
-        )
+    slides_placeholder = st.empty()
+    with slides_placeholder:
+        content = slide_row([image_name_to_path[x] for x in st.session_state.result])
+        click_detector(content, key="selected_slide")
 
+    if st.session_state.selected_slide is not None:
+        if st.session_state.selected_slide != st.session_state.prev_selected_slide:
+            st.session_state.selected_template = None
+        st.session_state.prev_selected_slide = st.session_state.selected_slide
         # Handle button clicks
-        index = int(st.session_state.selected_index)
+        index = int(st.session_state.selected_slide)
+        st.write(f"# Select a template for the Slide #{index + 1}")
+        click_detector(get_template_content(image_name_to_path), key="selected_template")
+
         st.write(f"index = {index}")
         st.write(f"template= {st.session_state.selected_template}")
-        st.write("-------")
 
         if st.session_state.selected_template is not None:
-            template = st.session_state.selected_template
-            if template == "Template1":
-                st.session_state.result[index] = "Template 1"
-            elif template == "Template2":
-                st.session_state.result[index] = "Template 2"
-            elif template == "Template3":
-                st.session_state.result[index] = "Template 3"
+            if st.session_state.selected_template != st.session_state.prev_selected_template:
+                st.session_state.prev_selected_template = st.session_state.selected_template
+                st.session_state.result[index] = st.session_state.selected_template
+                # slides_placeholder.empty()
+                st.experimental_rerun()
 
         st.write(st.session_state.result)
         print(st.session_state.result)
@@ -94,13 +89,15 @@ def main():
     page_1_placeholder = st.empty()
     page_2_placeholder = st.empty()
     if st.session_state.show_file_uploader:
-        document_upload_app(page_1_placeholder)
+        with page_1_placeholder.container():
+            document_upload_app()
     # Page 2: Hello World (displayed after processing is completed)
     if st.session_state.loading_complete:
         page_1_placeholder.empty()
         print(st.session_state)
         page_2_placeholder.header("Creating your presentation ...")
-        slides_app(page_2_placeholder)
+        with page_2_placeholder.container():
+            slides_app()
 
 
 def initialize_page():
@@ -108,9 +105,11 @@ def initialize_page():
     st.session_state.show_file_uploader = True
     st.session_state.initialized = True
     st.session_state.loading_complete = False
-    st.session_state.selected_index = None
+    st.session_state.selected_slide = None
+    st.session_state.prev_selected_slide = None
     st.session_state.selected_template = None
-    st.session_state.result = ["Template 1", "Template 2", "Template 3"]
+    st.session_state.prev_selected_template = None
+    st.session_state.result = list(image_name_to_path.keys())
 
 
 if __name__ == "__main__":
