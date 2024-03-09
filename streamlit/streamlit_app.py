7b0da257eff92a88317c752d68fea40aaa4a8aa5
Ajose Aishat <aishatajoseh@yahoo.com>
2024-03-09 19:45:18 +0000

    streamlit changes

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -3,9 +3,9 @@ from st_click_detector import click_detector
 import base64
 
 
-def convert_image():
+def convert_image(image_path):
     with open(
-        "/Users/shubhamjain/personal/aihackathon/aihackathon/streamlit/images/image1.png",
+        image_path,
         "rb",
     ) as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
@@ -15,23 +15,18 @@ image_paths = [
     "images/image1.png",
     "images/image2.png",
     "images/image3.png",
-    "images/image2.png",
-    "images/image3.png",
-    "images/image1.png",
-    "images/image2.png",
-    "images/image3.png",
-    "images/image2.png",
-    "images/image3.png",
 ]
 
-num_slides = 5
+num_slides = 3
 result = ["Template1"] * num_slides
 
 
 def slide_row(image_paths):
-    h1_html = "<h1></h1>"
+    h1_html = "<h1>Slides</h1>"
     for index, image_path in enumerate(image_paths):
-        h1_html += f'<span style="padding-left:20px, margin-bottom:10px"> <a href="#" id="{index}"><img  src="https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200" alt="Image" width="100"></a></span>'
+        imageBase64 = convert_image(image_path)
+        image_src = f'data:image/png;base64,{imageBase64}'
+        h1_html += f'<span style="padding-left:20px, margin-bottom:10px"> <a href="#" id="{index}"><img  src="{image_src}" alt="Image" width="100"></a></span>'
 
     h1_tag = f"<h1 style='overflow-X: auto; text-align: center; display: grid; grid-template-rows:repeat(auto-fit, minmax(200px, 1fr));grid-gap: 10px;'>{h1_html}</h1>"
     return h1_tag
@@ -49,8 +44,6 @@ def main():
     # Boolean flag to control visibility of file uploader
     show_file_uploader = True
 
-    selected_image = st.empty()
-
     # Page 1: Document Upload
     if show_file_uploader:
         uploaded_file = page1_placeholder.file_uploader(
@@ -64,53 +57,49 @@ def main():
             import time
 
             time.sleep(0.03)
-            st.progress(100)
+            progress_placeholder = st.progress(100)
 
             st.session_state.loading_complete = True
 
     # Page 2: Hello World (displayed after processing is completed)
     if getattr(st.session_state, "loading_complete", False):
+        
         # Hide Page 1 content
         page1_placeholder.empty()
+        progress_placeholder.empty()
 
         page2_placeholder.header("Creating your presentation ...")
-        page2_placeholder.write("Choose ...")
-
+        
         content = slide_row(image_paths)
-        clicked = click_detector(content)
-        # st.image('images/image1.png')
-        button_pressed = None
-        button_a = click_detector(
-            f"<a href='#' id='Template1'><img width='20%' src='data:image/png;base64,  {convert_image()}'></a>"
-        )
-        button_b = click_detector(
-            "<a href='#' id='Template2'><img width='20%' src='https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200'></a>"
-        )
-        button_c = click_detector(
-            "<a href='#' id='Template3'><img width='20%' src='https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200'></a>"
-        )
+        slides_placeholder = click_detector(content)
 
-        # Container to display button content and text entry
-        # container = st.empty()
+        selected_index = 0
+        if(slides_placeholder):
+            selected_index = slides_placeholder
+        
+        template1Image = convert_image("./images/image1.png")
+        template2Image = convert_image("./images/image2.png")
+        template3Image = convert_image("./images/image3.png")
+
+        template= click_detector(
+            f"<a href='#' id='Template1'><img width='100' src='data:image/png;base64,{template1Image}'></a><a href='#' id='Template2'><img width='100' src='data:image/png;base64,{template2Image}'></a><a href='#' id='Template2'><img width='100' src='data:image/png;base64,{template3Image}'></a>"
+        )
 
         # Handle button clicks
-        if button_a:
-            print(clicked)
-            print(int(clicked))
-            result[int(clicked)] = "Template1"
-        elif button_b:
-            result[int(clicked)] = "Template1"
-        elif button_c:
-            result[int(clicked)] = "Template1"
+        index = int(selected_index)
+        print(f'index = {index}')
+        print(f'template= {template}')
+        
+        if template == 'Template1':
+            st.session_state.result[index] = 'Template 1'
+        elif template == 'Template2':
+            st.session_state.result[index] = 'Template 2'
+        elif template == 'Template3':
+            st.session_state.result[index] = 'Template 3'
 
-        # if button_pressed:
-        #     container = st.container()
-        #     container.header("Container Text: {}".format(button_pressed))
-
-        # if clicked:
-        #     st.image(clicked, width=100)
-
-
+        st.write(st.session_state.result)
+        print(st.session_state.result)
+    
 if __name__ == "__main__":
+    st.session_state.result = ['Template 1', 'Template 2', 'Template 3']
     main()
-    print(result)
